1f180e78bae6170640d8b529df011b3ad576d77b
RonnieJr21 <rjhj@protonmail.com>
2025-10-28 21:14:12 -0400

    Remove target directory from repo

--- target/debug/RustServer.d
+++
@@ -1,1 +0,0 @@
-/home/rjhj/code/rust/RonniesPortfolio/target/debug/RustServer: /home/rjhj/code/rust/RonniesPortfolio/src/main.rs
--- target/debug/deps/RustServer-03f9421e17fd1d42.d
+++
@@ -1,5 +0,0 @@
-/home/rjhj/code/rust/RonniesPortfolio/target/debug/deps/RustServer-03f9421e17fd1d42.d: src/main.rs
-
-/home/rjhj/code/rust/RonniesPortfolio/target/debug/deps/RustServer-03f9421e17fd1d42: src/main.rs
-
-src/main.rs:
